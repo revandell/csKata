84ba90fa13594d9b772122a28d6964cda655fd7f
revandell <art.kurowski@gmail.com>
2019-09-28 11:32:44 +0200

    Dodaj pliki projektów.

---
+++ CodeWarsCata/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCata
+{
+    class Builder
+    {
+        static void Main(string[] args)
+        {
+            var pf = new PersonFactory();
+            var p1 = pf.CreatePerson("Aro");
+            var p2 = pf.CreatePerson("Vro");
+        }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class PersonFactory
+    {
+        private static int _id = 0;
+        public Person CreatePerson(string name)
+        {
+            Person p = new Person() { Name = name, Id = _id };
+            _id++;
+
+            return p;
+        }
+    }
+}
---
+++ CodeWarsCata/Ui.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCata
+{
+    public class Ui
+    {
+        public string GetUserInput(string message)
+        {
+            Console.WriteLine(message);
+            string userInput = Console.ReadLine();
+
+            return userInput;
+        }
+    }
+}
