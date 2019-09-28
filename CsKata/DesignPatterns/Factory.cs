e40fe0f898b2ecea805425b5858b29c798c7ea0d
revandell <art.kurowski@gmail.com>
2019-09-28 14:15:56 +0200

    zzz

---
+++ CsKata/DesignPatterns/Factory.cs
@@ -0,0 +1,23 @@
+﻿namespace CsKata.DesignPatterns
+{
+    public class Factory
+    {
+        public class Person
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class PersonFactory
+        {
+            private static int _id = 0;
+            public Person CreatePerson(string name)
+            {
+                Person p = new Person() { Name = name, Id = _id };
+                _id++;
+
+                return p;
+            }
+        }
+    }
+}
---
+++ CsKata/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using static CsKata.DesignPatterns.Builder;
+using static CsKata.DesignPatterns.Factory;
+
+namespace CsKata
+{
+    class Builder
+    {
+        static void Main(string[] args)
+        {
+            // use builder
+            var cb = new CodeBuilder("Person").AddField("Name", "string").AddField("Age", "int");
+            Console.WriteLine(cb);
+
+            // use factory
+            var pf = new PersonFactory();
+            var p1 = pf.CreatePerson("Aro");
+            var p2 = pf.CreatePerson("Vro");
+        }
+    }
+}
