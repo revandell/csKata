2ef25cb775206b2036a52ae4afd95d4b316d544a
revandell <art.kurowski@gmail.com>
2019-09-28 17:08:47 +0200

    Add Prototype pattern. Class Line implements method DeepCopy which returns its deep copy.

---
+++ CsKata/DesignPatterns/Prototype.cs
@@ -0,0 +1,23 @@
+﻿namespace CsKata.DesignPatterns
+{
+    class Prototype
+    {
+        public class Point
+        {
+            public int X, Y;
+        }
+
+        public class Line
+        {
+            public Point Start, End;
+
+            public Line DeepCopy()
+            {
+                Point newStart = new Point() { X = Start.X, Y = Start.Y };
+                Point newEnd = new Point() { X = End.X, Y = End.Y };
+
+                return new Line() { Start = newStart, End = newEnd };
+            }
+        }
+    }
+}
