48efea4eab0bc87216e922fa4947e7ac5819f812
revandell <art.kurowski@gmail.com>
2019-09-29 15:37:04 +0200

    Add class SingletonTester with function IsSingleton which tests if function func returns a singleton.

---
+++ CsKata/DesignPatterns/SingletonTester.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CsKata.DesignPatterns
+{
+    class SingletonTester
+    {
+        // checks if func() returns a singleton
+        public static bool IsSingleton(Func<object> func)
+        {
+            return func() == func();
+        }
+    }
+}
